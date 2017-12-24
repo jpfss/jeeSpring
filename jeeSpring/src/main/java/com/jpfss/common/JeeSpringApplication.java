1470bb981446a6a1585d17869c38844b3e90da4d
jpfss <873319522@qq.com>
2017-12-24 13:11:38 +0800

    升级到Springboot

---
+++ jeeSpring/src/main/java/com/jpfss/common/JeeSpringApplication.java
@@ -0,0 +1,12 @@
+package com.jpfss.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeeSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JeeSpringApplication.class, args);
+	}
+}
